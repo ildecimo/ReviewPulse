efa912c69c90248f6333319eb12b25db570fa3fb
Maksym Dmukhovskyy <dm.maksym@gmail.com>
2023-08-11 17:16:34 +0400

    Home page update

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -11,7 +11,13 @@ export const HomePage = () => {
       flexDirection="column"
       style={{ minHeight: '90vh' }}
     >
-      Home Page TBD
+      <h2 className="text-2xl">Home Page TBD</h2>
+
+      <span className="my-4 max-w-[350px] rounded-md border bg-orange-100 p-2 text-center">
+        Accessing the products page for the first time without visiting the
+        product review through the app extension will result in access token
+        error.
+      </span>
       <div className="mt-4">
         <NextLink href="/products">All Products</NextLink>
       </div>
