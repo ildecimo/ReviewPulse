0962ea05cdc7c7d21e79819ca4bf68a8caf208e7
Brad Adams <hi@breadadams.com>
2023-08-11 02:21:51 +0200

    feat: update review detail UI
    + Add initial customer box

--- src/components/ReviewDetail.tsx
+++ src/components/ReviewDetail.tsx
@@ -35,13 +35,12 @@ export const ReviewDetail = ({ product, review }: ReviewDetailProps) => {
               <div className="flex-1">
                 <div className="flex mb-2">
                   <div>
+                    <h1 className="text-base mt-0 mb-0.5">{review.title}</h1>
+
                     <StarRating rating={review.rating} />
 
-                    <h1 className="text-base my-0.5">{review.title}</h1>
-
-                    <p className="text-sm mt-0 mb-2 text-gray-500">
-                      {review.name}{' '}
-                      {Boolean(review.email) && `(${review.email})`}
+                    <p className="text-sm mt-0 text-gray-400">
+                      {convertToDateString(review.date_created)}
                     </p>
                   </div>
 
@@ -53,15 +52,32 @@ export const ReviewDetail = ({ product, review }: ReviewDetailProps) => {
                 <div className="bg-gray-100/80 text-gray-800 px-3 py-2 rounded-md">
                   {review.text}
                 </div>
-
-                <p className="text-sm mt-2 mb-0 text-gray-500 text-right">
-                  {convertToDateString(review.date_created)}
-                </p>
               </div>
             </div>
           </Box>
 
-          <Box border="box" padding="small" borderRadius="normal"></Box>
+          <Box border="box" padding="small" borderRadius="normal">
+            <div className="flex">
+              <div className="relative w-16 h-16 rounded-md overflow-hidden mr-4">
+                <Image
+                  alt={review.name}
+                  className="object-cover object-center"
+                  fill
+                  src={`https://unavatar.io/${review.email}?fallback=https://source.boringavatars.com/beam`}
+                  unoptimized
+                />
+              </div>
+              <div className="flex-1">
+                <div className="mb-2">
+                  <h2 className="text-base my-0.5">{review.name}</h2>
+
+                  {Boolean(review.email) && (
+                    <p className="text-sm mt-0 text-gray-500">{review.email}</p>
+                  )}
+                </div>
+              </div>
+            </div>
+          </Box>
         </div>
       </div>
     </div>
